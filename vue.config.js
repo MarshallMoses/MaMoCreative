d556a48c0e2a651f8e14e40cc801156b29fda064
MarshallMoses <Marshall.w.moses@gmail.com>
2022-07-26 23:02:53 -0700

    updating master

--- vue.config.js
+++ vue.config.js
@@ -4,3 +4,11 @@ module.exports = defineConfig({
 })
 
 module.exports = {  publicPath: process.env.NODE_ENV === "production" ? "mamocreative" : "/",};
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  }
+};
