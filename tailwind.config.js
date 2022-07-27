3acc3b0087af3686c060958d810599aa58a845cb
MarshallMoses <Marshall.w.moses@gmail.com>
2022-07-26 22:12:56 -0700

    First commit with vue and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
